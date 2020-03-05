5c7a9644c53e4b42ce7fa532657fb502ca8e14c0
Marjan Magharehi <marjan.magharehi@gmail.com>
2020-03-05 17:14:42 +0000

    Wrote questions for the chatbot to answer in an array

---
+++ controller/MainController.js
@@ -0,0 +1,11 @@
+// Write the questions for the chatbot to ask the user
+// an array of questions, that can be looped through
+
+var questions = [
+    'Hi, my name is Clara, what is your name?',
+    'Where are you from?',
+    'How old are you?',
+    'What tech stack are you currently working in?',
+    'Thank you for chatting with me! :)' 
+];
+
