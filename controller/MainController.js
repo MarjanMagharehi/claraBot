0bebffa45df5b5a1010a9867f47f5eca885e74b6
Marjan Magharehi <marjan.magharehi@gmail.com>
2020-03-05 17:27:51 +0000

    Wrote a function to loop through the questions from the bot and the user responses

--- controller/MainController.js
+++ controller/MainController.js
@@ -1,5 +1,5 @@
 // Write the questions for the chatbot to ask the user
-// an array of questions, that can be looped through
+// An array of questions, that can be looped through
 
 var questions = [
     'Hi, my name is Clara, what is your name?',
@@ -9,11 +9,44 @@ var questions = [
     'Thank you for chatting with me! :)' 
 ];
 
-// set the counter for looping through the array 
+// Set the counter for looping through the array 
 var counter = 0; 
 
-// logic to connect the HTML element to the logic in the document
+// Logic to connect the HTML element to the logic in the document
 var inputBox = document.querySelector("#answer");
 var output = document.querySelector("#result");
 // innerHTML changes the text inline, so the questions are asked in the same place
 output.innerHTML = questions[counter];
+
+/* Currently using if-else statements to loop through the questions and responses.
+Would like to move onto building in an API to have more of an actual 'AI' chatbot feel.
+Putting it in a function */ 
+
+function showResponse() {
+    var input = inputBox.value;
+    if(inputBox.value == "") {
+
+    }else {
+        if(counter == 0) {
+            output.innerHTML = `Hello ${input} nice to meet you`;
+            input = "";
+            counter++;
+            setTimeout(changeQuestion, 2000);
+        } else if(counter == 1) {
+            output.innerHTML = `Wow, ${input} is great`;
+            input = "";
+            counter++;
+            setTimeout(changeQuestion, 2000);
+        } else if(counter == 2) {
+            output.innerHTML = `Cool, that means you were born in ${2020 - input}`;
+            input = "";
+            counter++;
+            setTimeout(changeQuestion, 2000);
+        } else if(counter == 3) {
+            output.innerHTML = `Nice! ${input} is a good stack.`;
+            input = "";
+            counter++;
+            setTimeout(changeQuestion, 2000);
+        }
+    }
+}
