31916935ff8d5d472708bdfab965f1b67a4cd2d5
Marjan Magharehi <marjan.magharehi@gmail.com>
2020-03-11 11:58:13 +0000

    Added functionality to clear the input field on click

--- controller/MainController.js
+++ controller/MainController.js
@@ -18,7 +18,7 @@ var output = document.querySelector("#result");
 // innerHTML changes the text inline, so the questions are asked in the same place
 output.innerHTML = questions[counter];
 
-/* Currently using if-else statements to loop through the questions and responses.
+/* Currently using if-else statements to work through the questions and responses.
 Would like to move onto building in an API to have more of an actual 'AI' chatbot feel.
 Putting it in a function */ 
 
@@ -59,6 +59,10 @@ function showResponse() {
            
             }    
         }
+
+        // Given more time I would clean up this code, preparing less if-else statements to organise the questions for the chatbot to ask. 
+        // But also wanting to input more statements, so if the user input things like ages that don't make sense, there would be a response for it. Ultimately I would use ML so that
+        // it would not have to be in if-else statements.
 
 // function to change the question, when a response has been given.
 // if statement to break out of the questions given, and remove the input box
